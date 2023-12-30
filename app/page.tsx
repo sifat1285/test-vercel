dbdc2989810ae906d429cad304cb5b3fc9e7e17b
sifat1285 <m.sifat1666690@gmail.com>
2023-12-30 14:46:22 +0600

    refactor -- edited page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className=" h-full flex items-center justify-center">
-      hello home everyone
+      hello home everyone  idk what im doing, im just having fun
     </div>
   );
 }
